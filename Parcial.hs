0468bb8d8591a93f5df811bb3c9a4bfb22a8f01b
Maximo <maxipena.dev@gmail.com>
2025-05-05 14:10:04 -0300

    simulacro de parcial de Haskell

---
+++ Parcial.hs
@@ -0,0 +1,50 @@
+module Parcial where
+
+type Frase = [Char]
+type Mapeo = [(Char, Char)]
+type C = Char
+
+hayQueCodificar::Char -> [(Char, Char)] -> Bool
+hayQueCodificar _ [] = False -- > Da falso pq no hay más lista que recorrer
+hayQueCodificar x ((a,_):ys)
+    | x == a = True
+    | otherwise = hayQueCodificar x ys
+
+-- Ejercicio 2
+cuantasVecesHayQueCodificar::Char -> [Char] -> [(Char, Char)] -> Int 
+cuantasVecesHayQueCodificar c frase mapeo
+    | hayQueCodificar c mapeo == False = 0
+    | otherwise = cantidadDeVecesQueaparece c frase
+
+cantidadDeVecesQueaparece::Char -> [Char] -> Int 
+cantidadDeVecesQueaparece _ [] = 0
+cantidadDeVecesQueaparece c (x:xs)
+    | c == x = 1 + cantidadDeVecesQueaparece c xs
+    | otherwise = cantidadDeVecesQueaparece c xs 
+
+-- EJercicio 3
+-- Se supone que cada elemento de la frase esta en el mapeo por lo menos una vez
+-- requiere: {Existe al menos un c que pertenece a frase y hayQueCodificar(c, mapeo)=true} 
+laQueMasHayQueCodificar::[Char] -> [(Char, Char)] -> Char
+laQueMasHayQueCodificar frase mapeo = contador frase mapeo 0 ' ' -- > Siempre da uno diferente a '' ya que siempre por condicon hay un elemento q aparece por lo minimo una vez 
+
+-- laQueMasHayQueCodificar :: [Char] -> [(Char,Char)] -> Char
+contador::[Char] -> [(Char, Char)] -> Int -> Char -> Char 
+contador [] _ _ valor = valor 
+contador (x:xs) mapeo veces valor 
+    | cuenta > veces = contador xs mapeo (cuenta) x 
+    | otherwise = contador xs mapeo veces valor 
+    where 
+        cuenta = cuantasVecesHayQueCodificar x (x:xs) mapeo
+
+-- Ejercicio 4
+codificarFrase::[Char] -> [(Char, Char)] -> [Char] 
+codificarFrase [] _ = []
+codificarFrase (x:xs) mapeo 
+    | (hayQueCodificar x mapeo) = (codificar x mapeo) : codificarFrase xs mapeo -- > Vuelvo a mirar
+    | otherwise = codificarFrase xs mapeo -- > Miro el siguiente elemento
+
+codificar:: Char -> [(Char, Char)] -> Char 
+codificar c ((a,b):ys)
+    | c == a = b 
+    | otherwise = codificar c ys
---
+++ TestP.hs
@@ -0,0 +1,32 @@
+module TestP where
+
+import Test.HUnit 
+import Parcial
+
+
+runEjercicio2 = runTestTT testEj2
+runEj3 = runTestTT test3
+runEjercicio4 = runTestTT testEj2
+
+testEj2 = test [ 
+    "caso 2"~: cuantasVecesHayQueCodificar 'h' ['h','o','l','a'] [('h', 'o')] ~=? 1,
+    "sin la palabra en la frase" ~: cuantasVecesHayQueCodificar 'h' ['k','o','l','a'] [('h', 'o')] ~=? 0, -- > Pq no hay coincidencia
+    "sin la palabra en general"~: cuantasVecesHayQueCodificar 'h' ['k','o','l','a'] [('k', 'o')] ~=? 0, -- > Pq no esta en mapeo es 0,
+    "caso 2"~: cuantasVecesHayQueCodificar 'h' ['h', 'h' ,'o','l','a'] [('h', 'o')] ~=? 2
+    ]
+
+test3 = test [
+    "caso 1" ~: laQueMasHayQueCodificar ['h', 'o','l','a'] [('h', 'o')] ~=? 'h',
+    "aparece una vez" ~: laQueMasHayQueCodificar ['h','h','o','l','a'] [('h', 'o')] ~=? 'h', -- > Tiene que dar Hola pq es su primer elemento
+    "no coincide" ~: laQueMasHayQueCodificar ['h','o','o','l','a'] [('h', 'o')] ~=? 'h', -- > Da hola pq "como" no esta en el mapeo
+    "coincide" ~: laQueMasHayQueCodificar ['h','o','o','l','a'] [('o', 'o')] ~=? 'o',
+    "varias coicidencias" ~: laQueMasHayQueCodificar ['h','o','o','l','a'] [('h', 'o'),('o', 'o')] ~=? 'o', -- > Pq es el q mas se repite
+    "varias coicidencias al reves" ~: laQueMasHayQueCodificar ['h','o','o','l','a'] [('o', 'o'),('h', 'o')] ~=? 'o'
+    ]
+
+testEj4 = test [
+    "caso 1" ~: codificarFrase ['h', 'o','l','a'] [('h', 'o')] ~=? ['o', 'o','l','a'],
+    "multiples valores" ~: codificarFrase ['h', 'o','l','a'] [('h', 'o'), ('l', 'm')] ~=? ['o', 'o','m','a'],
+    "ningun valor" ~: codificarFrase ['h', 'o','l','a'] [('f', 'o'), ('g', 'm')] ~=? ['h', 'o','l','a'],
+    "cambio toda la frase" ~: codificarFrase ['h', 'o','l','a'] [('h', 'o'), ('l', 'm'), ('o', 'k'), ('a', 'z')] ~=? ['o', 'k','m','z']
+    ]
