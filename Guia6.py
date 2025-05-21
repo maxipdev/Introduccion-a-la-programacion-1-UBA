ea11f0857f74541e90e6f8fda265b13367d5308f
maxipdev <maxipena.dev@gmail.com>
2025-05-21 02:02:15 -0300

    cambios en la estructura

--- Guia6.py
+++ Guia6.py
@@ -1,44 +1,99 @@
-def suma(a: int, b: int) -> int:
-    res: int = a + b
-    return res
+import math
 
+# Ejericico 1
+def hola_mundo (): 
+    print("hola mundo")
 
-## Ejercicios clase 14/05/2025 
-def es_multiplo_de(n:int, m: int) -> bool: ## miro su n es multiplo de m
-    if n % m == 0: 
-        return True
-    else: return False
+hola_mundo()   
 
-def devolver_el_doble_si_es_par(n:int) -> int:
-    if n % 2 ==0 : return 2*n
-    else: return n 
+# 
+def imprimir_un_verso ():
+    verso = "Is this the real life?\nIs this just fantasy?\nCaught in a landslide,\nNo escape from reality."
+    print(verso)
 
-def grados(t: float) -> float: ## conversion de faren a cel
-    return (t - 32) * (5/9)
+imprimir_un_verso()
 
+# 
+def raiz_de_dos():
+    valor = math.sqrt(2)
+    valor_a_mostar = round(valor, 4)
+    print(valor_a_mostar)
 
-def es_primo(n:int) -> bool:
-    valor = True
-    n = abs(n) ## hago q N sea positivo
+raiz_de_dos()
 
-    if n == 0 or n == 1:
-        return False
-    
-    for i in range(2,n):
-        if n % i == 0: 
-            valor = False ## pq se hace q haya un divisor
-            break
-    return valor
+# 
+def factorial_de_dos():
+    print(2*1)
 
-def cuantos_primos_en_rango(n:int, m: int) -> int:
-    minimo = min(n,m)
-    maximo = max(n,m)
+factorial_de_dos()
 
-    contador: int = 0
-    for i in range(minimo, maximo + 1):
-        if es_primo(i): contador += 1
+# 
+def perimetro():
+    return  2*(math.pi)
 
-    return contador
+perimetro()
 
-print(cuantos_primos_en_rango(5,3))
-print(cuantos_primos_en_rango(3,5))
+# Ejercico 2
+def imprimir_saludo(nombre: str) -> str:
+    print(f'Hola {nombre}, como estas?')
+
+imprimir_saludo("abumeyang")
+
+#
+def raiz_cuadrada_de_un_numero(num: int) -> float:
+    operacion = math.sqrt(num)
+    print(operacion)
+
+raiz_cuadrada_de_un_numero(2)
+
+#
+def fahrenheit_a_celsius(t: float) -> float:
+    return ((t-32)*5)/9
+
+temperatura = fahrenheit_a_celsius(90)
+print(temperatura)
+
+# 
+def imprimir_dos_veces(estribillo: str) -> str:
+    print(estribillo*2)
+
+imprimir_dos_veces("me gusta la noche...")
+
+#
+def es_multiplo_de(n: int, m: int) -> bool:
+    ## Buscamos ver si n es un multiplo de m
+    ## tomo sus absolutos ya que habla de enteros
+    n = abs(n)
+    m = abs(m)
+    for k in range(1, n):
+        cuenta = m*k
+        if cuenta > n: return False ## pq la cuenta es mas grande que n lo que hace que n no pueda ser un multiplo de ese numero 
+        elif n == cuenta: return True ## sie encontro algun multiplo
+    return False ## en caso de que no encuentre ningun multiplo
+
+print("pruebo multiplos")
+print(es_multiplo_de(2,2))
+print(es_multiplo_de(4,2))
+print(es_multiplo_de(3,2))
+print(es_multiplo_de(17,8))
+
+#
+def es_par(n: int) -> bool:
+    return es_multiplo_de(n, 2) ## solo pq el ejercicio lo pide que hay que usar esta función
+
+print("pruebo pares")
+print(es_par(17))
+print(es_par(8))
+
+#
+## cantidad de pizzas necesarias para que cada comensal coma esa cantidad de porciones como minimo
+def cantidad_de_pizzas(comensales: int, cant_de_porciones: int):
+    porciones_pizza = 8 ## 8 porciones por pizza
+    cuenta = (comensales * cant_de_porciones)/porciones_pizza
+    return math.ceil(cuenta) ## siempre da pizzas por demas como pide
+
+print("pruebo pizzas")
+print(cantidad_de_pizzas(2,4))
+print(cantidad_de_pizzas(4,4))
+print(cantidad_de_pizzas(8,4))
+print(cantidad_de_pizzas(4,5))
